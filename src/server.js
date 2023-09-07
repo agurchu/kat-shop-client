80e8273bf61516f44ec3d9e38b4cb2a970727f4d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-09-07 10:06:46 +0200

    update server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/";
+export const server = "kat-shop-api.up.railway.app/api/v2";
+export const backend_url = "kat-shop-api.up.railway.app";
